76448a09df711b2a81a03245ffb076d5ccaa63c7
ilusta <ilust.d@gmail.com>
2025-09-10 23:37:20 +0300

    CubeMars GL35 coeffs

--- src/main.cpp
+++ src/main.cpp
@@ -2,9 +2,9 @@
 #include "AS5600.h"
 #include "MagneticSensorAS5600.h"
 //#include <pinout.h>
-float max_current = 1.35;
+float max_current = 3.0;
 // (Pole_pairs, resistance, kv, inductanse)
-BLDCMotor motor = BLDCMotor(14, 4.5, 63.5, 0.0008); 
+BLDCMotor motor = BLDCMotor(7, 3.6, 82.5, 0.00021); 
 uint32_t last_receive_timer = 0;
 BLDCDriver6PWM driver = BLDCDriver6PWM(PA8, PC13, PA9, PA12, PA10, PB15); 
 //MagneticSensorI2C sensor = MagneticSensorI2C(AS5600_I2C);
@@ -15,12 +15,14 @@ double sen_angle = 0;
 int8_t sen_dir = 0;
 bool motor_callibrated = false;
 bool motor_disabled = false;
+
 void onMotor(char* cmd){ 
   float new_speed = 0;
   commander.scalar(&new_speed, cmd);
   motor.target = float(new_speed);
   last_receive_timer = millis();
 }
+
 void START_STOP(char* cmd){ 
   float _start_stop_data = 0;
   commander.scalar(&_start_stop_data, cmd);
@@ -65,8 +67,10 @@ void START_STOP(char* cmd){
       motor_disabled = false;
   }
 }
-bool test = false, use = false;
- void setup() {
+
+bool test = true, use = true;
+
+void setup() {
   Wire.setClock(1000000);
   Serial.begin(115200);
   new_sensor.closeTransactions = true;
@@ -75,7 +79,7 @@ bool test = false, use = false;
   motor.useMonitoring(Serial);
   // driver config
   // power supply voltage [V]
-  driver.voltage_power_supply = 17.4;
+  driver.voltage_power_supply = 24.0;
   driver.pwm_frequency = 14000;
   driver.init();
 
@@ -92,7 +96,7 @@ bool test = false, use = false;
   motor.monitor_downsample = 300;
  // motor.LPF_current_d.Tf = 0.01;
   //motor.LPF_current_q.Tf = 0.01;
-  motor.voltage_limit = 8;   // [V]
+  motor.voltage_limit = 20;   // [V]
   motor.current_limit = max_current;
   motor.velocity_limit = 10000; // [rad/s]
   // default P=0.5 I = 10 D = 0
@@ -123,9 +127,9 @@ commander.add('Z', START_STOP, "motor");
 current_sense.init();
 motor.linkCurrentSense(&current_sense);
 motor.init();
-use = true;
-test = false;
-uint8_t motor_num = 3, robot = 2;
+use = false;
+test = true;
+uint8_t motor_num = 1, robot = 1;
 if(!test)
 {
   if(robot == 1)
@@ -155,34 +159,6 @@ if(!test)
       break;
     }
   }
-
-  if(robot == 2)
-  {
-    switch (motor_num)
-    {
-      case 1:
-      motor.sensor_direction = Direction::CCW;
-    motor.zero_electric_angle = 5.88;
-    commander.add('A', onMotor, "motor");
-    break;
-      case 2:
-      motor.sensor_direction = Direction::CW;
-    motor.zero_electric_angle = 6.25;    
-    commander.add('B', onMotor, "motor");
-    break;
-
-    case 3:
-    motor.sensor_direction = Direction::CW;
-    motor.zero_electric_angle = 1.35;
-    commander.add('C', onMotor, "motor");
-      break;
-    case 4:
-    motor.sensor_direction = Direction::CCW;
-    motor.zero_electric_angle = 3.06;
-    commander.add('D', onMotor, "motor");
-      break;
-    }
-  }
 }
 else
 {
@@ -190,7 +166,10 @@ else
 }
 motor.initFOC();
 }
+
+
 uint32_t timer1 = 0, timer2 = 0;
+
 void loop() {
   motor.loopFOC();
   //motor.move(10);
@@ -229,15 +208,23 @@ void loop() {
     }
    if(use == false)
    {
-    if(millis() - last_receive_timer < 4000)
-    {
+    // if(millis() - last_receive_timer < 10000)
+    // {
       motor.current_limit = max_current;
-      motor.PID_velocity.P = 0.75;
-      motor.PID_velocity.I = 0.001;
-      motor.PID_velocity.D = 0.0035; 
+      motor.PID_velocity.P = 1;
+      motor.PID_velocity.I = 5;
+      // motor.PID_velocity.D = 0; 
       motor.velocity_limit = 10000;
       motor.PID_velocity.output_ramp = 1000;
-      motor.move(20);
+
+      static float recievedSpeed = 0;
+      if(Serial.available()){
+        int newSpeed = Serial.parseInt();
+        if(newSpeed > -2500 && newSpeed < 2500)
+          recievedSpeed = newSpeed * 0.10466;
+      }
+
+      motor.move(recievedSpeed);
       // motor.current_limit = max_current;
       // motor.PID_velocity.P = 0.75;
       // motor.PID_velocity.I = 0.1;
@@ -245,11 +232,11 @@ void loop() {
       // motor.velocity_limit = 10000;
       // motor.PID_velocity.output_ramp = 1000;
       //  motor.move(2);
-    }
-    else if(millis() - last_receive_timer < 8000)
+    /*}
+    else if(millis() - last_receive_timer < 20000)
     {
-      motor.move(-6);
-      }
+      motor.move(-1*0.10466);
+    }
       else if(millis() - last_receive_timer < 1000)
     {
     // motor.LPF_velocity = 0.06;
@@ -272,6 +259,7 @@ void loop() {
       }
       else
         last_receive_timer = millis();
+        */
     motor.monitor();
    }
   }
