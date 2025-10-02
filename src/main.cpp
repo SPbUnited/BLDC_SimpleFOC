1565d655dc1da8beb0a2d7474c5a80d109810191
BorisVikVik <boris@victorov.com>
2025-10-02 14:27:35 +0300

    First commit for ecmax. Hall sensors and parametrs.

--- src/main.cpp
+++ src/main.cpp
@@ -1,15 +1,19 @@
 #include <SimpleFOC.h>
-#include "AS5600.h"
-#include "MagneticSensorAS5600.h"
+// #include "AS5600.h"
+#include <SimpleFOCDrivers.h>
+#include "encoders/MXLEMMING_observer/MXLEMMINGObserverSensor.h"
+// #include "MagneticSensorAS5600.h"
 //#include <pinout.h>
-float max_current = 3.0;
+float max_current = 2;
 // (Pole_pairs, resistance, kv, inductanse)
-BLDCMotor motor = BLDCMotor(7, 3.6, 82.5, 0.00021); 
+BLDCMotor motor = BLDCMotor(1, 0.955, 1050, 0.0498 * 0.001); 
 uint32_t last_receive_timer = 0;
 BLDCDriver6PWM driver = BLDCDriver6PWM(PA8, PC13, PA9, PA12, PA10, PB15); 
 //MagneticSensorI2C sensor = MagneticSensorI2C(AS5600_I2C);
-MagneticSensorAS5600 new_sensor;
-LowsideCurrentSense current_sense = LowsideCurrentSense(0.003f, -64.0f/7.0f, A_OP1_OUT, A_OP2_OUT, A_OP3_OUT);
+// MagneticSensorAS5600 new_sensor;
+HallSensor new_sensor(PB6,PB7,PB8,1);
+// MXLEMMINGObserverSensor new_sensor = MXLEMMINGObserverSensor(motor);
+LowsideCurrentSense current_sense = LowsideCurrentSense(0.003f, -20.0f, A_OP1_OUT, A_OP2_OUT, A_OP3_OUT);
 Commander commander = Commander(Serial, 'e', false);
 double sen_angle = 0;
 int8_t sen_dir = 0;
@@ -23,63 +27,63 @@ void onMotor(char* cmd){
   last_receive_timer = millis();
 }
 
-void START_STOP(char* cmd){ 
-  float _start_stop_data = 0;
-  commander.scalar(&_start_stop_data, cmd);
-  int motor_enable = _start_stop_data;
-  if(_start_stop_data == 1.0)
-  {
-    motor.current_limit = max_current;
-    motor.sensor_direction = Direction::UNKNOWN;
-    motor.initFOC();
-    motor.controller = MotionControlType::velocity;
-    motor_callibrated = true;
-    if(motor.sensor_direction == Direction::CW)
-      sen_dir = 1;
-    else
-      sen_dir = -1;
-    sen_angle = motor.zero_electric_angle;
+// void START_STOP(char* cmd){ 
+//   float _start_stop_data = 0;
+//   commander.scalar(&_start_stop_data, cmd);
+//   int motor_enable = _start_stop_data;
+//   if(_start_stop_data == 1.0)
+//   {
+//     motor.current_limit = max_current;
+//     motor.sensor_direction = Direction::UNKNOWN;
+//     motor.initFOC();
+//     motor.controller = MotionControlType::velocity;
+//     motor_callibrated = true;
+//     if(motor.sensor_direction == Direction::CW)
+//       sen_dir = 1;
+//     else
+//       sen_dir = -1;
+//     sen_angle = motor.zero_electric_angle;
     
-  }
-  else if(_start_stop_data == 0.0)
-  {
-    if(motor_disabled == false)
-    {
-      motor.current_limit = 0;
-      motor.controller = MotionControlType::torque;
-      motor.torque_controller = TorqueControlType::foc_current;
-      motor.LPF_current_d.Tf = 0.01;
-      motor.LPF_current_q.Tf = 0.01;
-        motor_disabled = true;
-        motor.target = 0;
-       // motor.spe
-    }
-  }
-  else if(_start_stop_data = 2.0)
-  {
-      motor.current_limit = max_current;
-      motor.controller = MotionControlType::velocity;
-      motor.torque_controller = TorqueControlType::voltage;
-      motor.LPF_current_d.Tf = 0.01;
-      motor.LPF_current_q.Tf = 0.01;
-        motor_disabled = true;
-        motor.target = 0;
-      motor_disabled = false;
-  }
-}
+//   }
+//   else if(_start_stop_data == 0.0)
+//   {
+//     if(motor_disabled == false)
+//     {
+//       motor.current_limit = 0;
+//       motor.controller = MotionControlType::torque;
+//       motor.torque_controller = TorqueControlType::foc_current;
+//       motor.LPF_current_d.Tf = 0.01;
+//       motor.LPF_current_q.Tf = 0.01;
+//         motor_disabled = true;
+//         motor.target = 0;
+//        // motor.spe
+//     }
+//   }
+//   else if(_start_stop_data = 2.0)
+//   {
+//       motor.current_limit = max_current;
+//       motor.controller = MotionControlType::velocity;
+//       motor.torque_controller = TorqueControlType::voltage;
+//       motor.LPF_current_d.Tf = 0.01;
+//       motor.LPF_current_q.Tf = 0.01;
+//         motor_disabled = true;
+//         motor.target = 0;
+//       motor_disabled = false;
+//   }
+// }
 
-bool test = true, use = true;
+bool test = true, use = false;
 
 void setup() {
   Wire.setClock(1000000);
   Serial.begin(115200);
-  new_sensor.closeTransactions = true;
-  new_sensor.useHysteresis = false;
+  // new_sensor.closeTransactions = true;
+  // new_sensor.useHysteresis = false;
   new_sensor.init();
   motor.useMonitoring(Serial);
   // driver config
   // power supply voltage [V]
-  driver.voltage_power_supply = 24.0;
+  driver.voltage_power_supply = 12.0;
   driver.pwm_frequency = 14000;
   driver.init();
 
@@ -91,27 +95,27 @@ void setup() {
   // limiting motor movements
   motor.foc_modulation = FOCModulationType::SpaceVectorPWM;
   motor.torque_controller = TorqueControlType::foc_current;
- // motor.monitor_variables = _MON_CURR_D|_MON_CURR_Q;
-  motor.monitor_variables = _MON_VEL;
+ motor.monitor_variables = _MON_TARGET|_MON_ANGLE|_MON_VEL;
+  // motor.monitor_variables = _MON_CURR_Q;
   motor.monitor_downsample = 300;
  // motor.LPF_current_d.Tf = 0.01;
   //motor.LPF_current_q.Tf = 0.01;
-  motor.voltage_limit = 20;   // [V]
+  motor.voltage_limit = 12;   // [V]
   motor.current_limit = max_current;
   motor.velocity_limit = 10000; // [rad/s]
   // default P=0.5 I = 10 D = 0
 motor.PID_velocity.P = 0.7;
-motor.PID_velocity.I = 7;
-motor.PID_velocity.D = 0.0035; //0.0035
+motor.PID_velocity.I = 0.1;
+motor.PID_velocity.D = 0;//0.0035; //0.0035
 
 
-motor.PID_current_d.P = 2.75;
-motor.PID_current_d.I = 350;
+motor.PID_current_d.P = 1;//2.75;
+motor.PID_current_d.I = 1;
 motor.PID_current_d.D = 0;
 motor.LPF_current_d = 0.002f;
 
-motor.PID_current_q.P = 2.75;
-motor.PID_current_q.I = 350;
+motor.PID_current_q.P = 1;//2.75;
+motor.PID_current_q.I = 1;
 motor.PID_current_q.D = 0;
 motor.LPF_current_q = 0.002f;
 
@@ -123,7 +127,7 @@ motor.LPF_current_q = 0.002f;
   // init motor hardware
 motor.linkSensor(&new_sensor);
 //A - is the ID of drive
-commander.add('Z', START_STOP, "motor");
+
 current_sense.init();
 motor.linkCurrentSense(&current_sense);
 motor.init();
@@ -165,6 +169,10 @@ else
   delay(7000);
 }
 motor.initFOC();
+
+Serial.println("Motor ready.");
+Serial.println("Torque mode.");
+
 }
 
 
@@ -177,7 +185,7 @@ void loop() {
   // if(timer2 == 100)
   // {
   //   timer2 = 0;
-  //   Serial.println(new_sensor.getAngle());
+    // Serial.println("goida");0.2
   // }
    if(!motor_disabled)
    {
@@ -211,9 +219,9 @@ void loop() {
     // if(millis() - last_receive_timer < 10000)
     // {
       motor.current_limit = max_current;
-      motor.PID_velocity.P = 1;
-      motor.PID_velocity.I = 5;
-      // motor.PID_velocity.D = 0; 
+      motor.PID_velocity.P = 0.05;
+      motor.PID_velocity.I = 0;
+      motor.PID_velocity.D = 0; 
       motor.velocity_limit = 10000;
       motor.PID_velocity.output_ramp = 1000;
 
@@ -222,8 +230,13 @@ void loop() {
         int newSpeed = Serial.parseInt();
         if(newSpeed > -2500 && newSpeed < 2500)
           recievedSpeed = newSpeed * 0.10466;
+        // float newSpeed = Serial.parseFloat();
+        // if(newSpeed < -2.0 && newSpeed > 2.0)
+        // {newSpeed = 2.0 * (newSpeed/abs(newSpeed));}
+        // recievedSpeed = newSpeed;
+        Serial.println(recievedSpeed);
       }
-
+      // Serial.println(new_sensor.getFullRotations());
       motor.move(recievedSpeed);
       // motor.current_limit = max_current;
       // motor.PID_velocity.P = 0.75;
@@ -260,7 +273,7 @@ void loop() {
       else
         last_receive_timer = millis();
         */
-    motor.monitor();
+    // motor.monitor();
    }
   }
   commander.run();
