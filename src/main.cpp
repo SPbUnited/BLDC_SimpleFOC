16c0547ef306582a9fe51cf5bfd60755af03b211
BorisVikVik <boris@victorov.com>
2025-11-20 02:37:38 +0300

    working analogread and blank can

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,7 @@
 #include "SimpleFOCDrivers.h"
 #include <SimpleFOC.h>
 #include <SPI.h>
-
+#include <SimpleCan.h>
 #define SPI_SR_TXP
 
 // #include "./STM32_CAN/STM32_CAN.h"
@@ -11,6 +11,11 @@
 // #include "encoders/MXLEMMING_observer/MXLEMMINGObserverSensor.h"
 //#include <pinout.h>
 // STM32_CAN Can1( PA11, PB9 );
+
+static void handleCanMessage(FDCAN_RxHeaderTypeDef rxHeader, uint8_t *rxData);
+static void init_CAN(void);
+SimpleCan can1(/*terminateTransceiver:*/true);
+SimpleCan::RxHandler can1RxHandler(8, handleCanMessage);
 // SPIClass *hspi = NULL;
 // static CAN_message_t CAN_RX_msg;
 
@@ -121,7 +126,7 @@ void setup() {
   // driver config
   // power supply voltage [V]
   driver.voltage_power_supply = 24.0;
-  driver.voltage_limit = 10;
+  driver.voltage_limit = 4;
   driver.pwm_frequency = 14000;
   driver.init();
 
@@ -166,12 +171,13 @@ motor.LPF_current_q = 0.002f;
   motor.LPF_velocity = 0.3;
   // motor.PID_velocity.output_ramp = 1000;
   // motor.motion_downsample = 3;
+
   // init motor hardware
 motor.linkSensor(&new_sensor);
 //A - is the ID of drive
 // commander.add('Z', START_STOP, "motor");
 motor.linkCurrentSense(&current_sense);
-motor.sensor_direction= Direction::UNKNOWN;
+motor.sensor_direction= Direction::CCW;
 
 motor.init();
 current_sense.init();
@@ -183,77 +189,135 @@ test_motor = true;
 uint8_t motor_num = 1, robot = 1;
 motor.initFOC();
 pinMode(pinNametoDigitalPin(PC_6), OUTPUT);
+// pinMode(PB14, INPUT_ANALOG);
+// pinMode(PA4, INPUT_ANALOG);
 // digitalPin
 digitalWrite(pinNametoDigitalPin(PC_6), LOW);
+init_CAN();
 }
 
+static void init_CAN()
+{
+	// Serial1.println(
+  can1.init(CanSpeed::Kbit250);
+    //  == HAL_OK
+	// 	? "CAN: initialized."
+	// 	: "CAN: error when initializing.");
+
+	FDCAN_FilterTypeDef sFilterConfig;
+
+	// Configure Rx filter
+	sFilterConfig.IdType = FDCAN_STANDARD_ID;
+	sFilterConfig.FilterIndex = 0;
+	sFilterConfig.FilterType = FDCAN_FILTER_MASK;
+	sFilterConfig.FilterConfig = FDCAN_FILTER_DISABLE;
+	sFilterConfig.FilterID1 = 0x321;
+	sFilterConfig.FilterID2 = 0x7FF;
+
+	can1.configFilter(&sFilterConfig);
+	can1.configGlobalFilter(FDCAN_ACCEPT_IN_RX_FIFO0, FDCAN_ACCEPT_IN_RX_FIFO0, FDCAN_FILTER_REMOTE, FDCAN_FILTER_REMOTE);
+	can1.activateNotification(&can1RxHandler);
+	
+	// Serial1.println(
+  can1.start();
+  //  == HAL_OK
+	// 	? "CAN: started."
+	// 	: "CAN: error when starting.");
+}
+
+static void handleCanMessage(FDCAN_RxHeaderTypeDef rxHeader, uint8_t *rxData)
+{
+	// if ((rxHeader.Identifier != 0x321) || (rxHeader.IdType != FDCAN_STANDARD_ID) || (rxHeader.DataLength != FDCAN_DLC_BYTES_2))
+	// {
+	// 	return;
+	// }
+
+	digitalToggle(LED_BUILTIN);
+}
 
 uint32_t timer1 = 0, timer2 = 0;
 int8_t flag = 0;
+double b14=0, a0=0, a4=0;
 void loop() {
   motor.loopFOC();
-      motor.current_limit = max_current;
-      // motor.PID_velocity.P = 1;
-      // motor.PID_velocity.I = 10;
-      // motor.PID_velocity.D = 0; 
-      motor.velocity_limit = 10000;
-      // motor.PID_velocity.output_ramp = 1000;
-      // motor.move(10);
-      // static float recievedSpeed = 0;
-      // // motor.move(3.14);
-      if(millis() - timer1 >= 5000)
-      {
-        flag += 1;
-        if (flag >= 3)
-          flag = 0;
-        timer1 = millis();
-      }
-      if (flag == 0)
-      {
-          motor.move(-7);
-      }
-      else if (flag == 1)
-      {
-        motor.move(0);
-      }
-      else
-      {
-        motor.move(7);
-      }
-      // motor.monitor();
+  // b14 = analogRead(PB14);
+  b14 = _readADCVoltageLowSide(A_VBUS, current_sense.params) * 9.2;
+  double analog = _readADCVoltageLowSide(A_TEMPERATURE, current_sense.params);
+  analog = ((float)((1 << 12) - 1) / ((4096.0f/3.3f)*analog) - 1.0f) / 2.12;
+  analog = (log(analog) / 3900.0f) + 1.0f / (25.0f + 273.15f);
+  a0 = 1.0f / analog - 273.15f;
+  analog = _readADCVoltageLowSide(PA4, current_sense.params);
+  analog = ((float)((1 << 12) - 1) / ((4096.0f/3.3f)*analog) - 1.0f) / 2.12;
+  analog = (log(analog) / 3900.0f) + 1.0f / (25.0f + 273.15f);
+  a4 = 1.0f / analog - 273.15f;
+  // analog =  4700 * (4096 / ((4096/3.3) * analog) - 1.0);
+  // // analog = 0.47 / ((float)((1 << 12) - 1) / analog - 1.0f);
+  // analog = 1.0f/(298.15) + (1.0f/3380) * (log(analog/10000));// + 1.0f / (25 + 273.15f);
+  // a0 = 1.0f / analog - 273.15f;
+  // a0 = (1.0f / analog - 273.15f);
+  uint32_t tmp_an = a0 + b14 + a4;
+  //   motor.current_limit = max_current;
+  //   // motor.PID_velocity.P = 1;
+  //   // motor.PID_velocity.I = 10;
+  //   // motor.PID_velocity.D = 0; 
+  //   motor.velocity_limit = 10000;
+  //   // motor.PID_velocity.output_ramp = 1000;
+  //   // motor.move(10);
+  //   // static float recievedSpeed = 0;
+  //   // // motor.move(3.14);
+    if(millis() - timer1 >= 3000)
+    {
+      flag += 1;
+      if (flag >= 3)
+        flag = 0;
+      timer1 = millis();
+    }
+    if (flag == 0)
+    {
+        motor.move(50);
+    }
+    else if (flag == 1)
+    {
+      motor.move(0);
+    }
+    else
+    {
+      motor.move(-50);
+    }
+    // motor.monitor();
 
-    //   if (Can1.read(CAN_RX_msg) ) {
-    //     // Serial.print("Channel:");
-    //     // Serial.print(CAN_RX_msg.bus);
-    //     if (CAN_RX_msg.flags.extended == false) {
-    //       // Serial.print(" Standard ID:");
-    //     }
-    //     else {
-    //       // Serial.print(" Extended ID:");
-    //     }
-    //     // Serial.print(CAN_RX_msg.id, HEX);
+  //   if (Can1.read(CAN_RX_msg) ) {
+  //     // Serial.print("Channel:");
+  //     // Serial.print(CAN_RX_msg.bus);
+  //     if (CAN_RX_msg.flags.extended == false) {
+  //       // Serial.print(" Standard ID:");
+  //     }
+  //     else {
+  //       // Serial.print(" Extended ID:");
+  //     }
+  //     // Serial.print(CAN_RX_msg.id, HEX);
 
-    // #if defined(HAL_FDCAN_MODULE_ENABLED)
-    //     if(CAN_RX_msg.flags.fd_rateswitch) {
-    //       // Serial.println(" [FD-BRS]");
-    //     }
-    //     else if(CAN_RX_msg.flags.fd_frame) {
-    //       // Serial.println(" [FD]");
-    //     }
-    // #endif
+  // #if defined(HAL_FDCAN_MODULE_ENABLED)
+  //     if(CAN_RX_msg.flags.fd_rateswitch) {
+  //       // Serial.println(" [FD-BRS]");
+  //     }
+  //     else if(CAN_RX_msg.flags.fd_frame) {
+  //       // Serial.println(" [FD]");
+  //     }
+  // #endif
 
-    //     // Serial.print(" DLC: ");
-    //     // Serial.print(CAN_RX_msg.len);
-    //     if (CAN_RX_msg.flags.remote == false) {
-    //       // Serial.print(" buf: ");
-    //       for(int i=0; i<CAN_RX_msg.len; i++) {
-    //         // Serial.print("0x"); 
-    //         // Serial.print(CAN_RX_msg.buf[i], HEX); 
-    //         if (i != (CAN_RX_msg.len-1))  Serial.print(" ");
-    //       }
-    //       // Serial.println();
-    //     } else {
-    //       // Serial.println(" Data: REMOTE REQUEST FRAME");
-    //     }
-    // }
+  //     // Serial.print(" DLC: ");
+  //     // Serial.print(CAN_RX_msg.len);
+  //     if (CAN_RX_msg.flags.remote == false) {
+  //       // Serial.print(" buf: ");
+  //       for(int i=0; i<CAN_RX_msg.len; i++) {
+  //         // Serial.print("0x"); 
+  //         // Serial.print(CAN_RX_msg.buf[i], HEX); 
+  //         // if (i != (CAN_RX_msg.len-1))  Serial.print(" ");
+  //       }
+  //       // Serial.println();
+  //     } else {
+  //       // Serial.println(" Data: REMOTE REQUEST FRAME");
+  //     }
+  // }
 }
