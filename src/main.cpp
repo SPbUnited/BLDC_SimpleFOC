2178b17df5adaede4d47ba5a934e6680cab76842
BorisVikVik <boris@victorov.com>
2025-12-09 16:28:42 +0300

    Added  pull up

--- src/main.cpp
+++ src/main.cpp
@@ -5,9 +5,9 @@
 #include "encoders/MXLEMMING_observer/MXLEMMINGObserverSensor.h"
 // #include "MagneticSensorAS5600.h"
 //#include <pinout.h>
-float max_current = 1.0;
+float max_current = 2.0;
 // (Pole_pairs, resistance, kv, inductanse)
-BLDCMotor motor = BLDCMotor(2, 0.94, 770, 0.104 * 0.001); 
+BLDCMotor motor = BLDCMotor(1, 0.94, 770, 0.104 * 0.001); 
 uint32_t last_receive_timer = 0;
 BLDCDriver6PWM driver = BLDCDriver6PWM(PA8, PC13, PA9, PA12, PA10, PB15); 
 //MagneticSensorI2C sensor = MagneticSensorI2C(AS5600_I2C);
@@ -16,18 +16,18 @@ BLDCDriver6PWM driver = BLDCDriver6PWM(PA8, PC13, PA9, PA12, PA10, PB15);
 HallSensor new_sensor(PB6,PB7,PB8,1);
 // MXLEMMINGObserverSensor new_sensor = MXLEMMINGObserverSensor(motor);
 LowsideCurrentSense current_sense = LowsideCurrentSense(0.003f, -9.0f, A_OP1_OUT, A_OP2_OUT, A_OP3_OUT);
-Commander commander = Commander(Serial, 'e', false);
+// Commander commander = Commander(Serial, 'e', false);
 double sen_angle = 0;
 int8_t sen_dir = 0;
 bool motor_callibrated = false;
 bool motor_disabled = false;
 
-void onMotor(char* cmd){ 
-  float new_speed = 0;
-  commander.scalar(&new_speed, cmd);
-  motor.target = float(new_speed);
-  last_receive_timer = millis();
-}
+// void onMotor(char* cmd){ 
+//   float new_speed = 0;
+//   commander.scalar(&new_speed, cmd);
+//   motor.target = float(new_speed);
+//   last_receive_timer = millis();
+// }
 
 // void START_STOP(char* cmd){ 
 //   float _start_stop_data = 0;
@@ -77,12 +77,10 @@ void onMotor(char* cmd){
 bool test = true, use = false;
 
 void setup() {
-  Wire.setClock(1000000);
-  Serial.begin(115200);
   // new_sensor.closeTransactions = true;
   // new_sensor.useHysteresis = false;
+  new_sensor.pullup = Pullup::USE_INTERN;
   new_sensor.init();
-  motor.useMonitoring(Serial);
   // driver config
   // power supply voltage [V]
   driver.voltage_power_supply = 24.0;
@@ -94,7 +92,7 @@ void setup() {
   motor.linkDriver(&driver);
   current_sense.skip_align = true;
   current_sense.linkDriver(&driver);
-  motor.sensor_direction = Direction::CW;
+  // motor.sensor_direction = Direction::CW;
   // limiting motor movements
   motor.foc_modulation = FOCModulationType::SpaceVectorPWM;
   motor.torque_controller = TorqueControlType::foc_current;
@@ -108,7 +106,7 @@ void setup() {
   motor.velocity_limit = 10000; // [rad/s]
   // default P=0.5 I = 10 D = 0
 motor.PID_velocity.P = 1.5;
-motor.PID_velocity.I = 0;
+motor.PID_velocity.I = 0.1;
 motor.PID_velocity.D = 0;//0.001;//0.0035; //0.0035
 
 
@@ -118,22 +116,23 @@ motor.PID_current_d.D = 0;
 motor.LPF_current_d = 0.002f;
 
 motor.PID_current_q.P = 0.7;//2.75;
-motor.PID_current_q.I = 15;
+motor.PID_current_q.I = 1;
 motor.PID_current_q.D = 0;
 motor.LPF_current_q = 0.002f;
 
   // open loop control config
   motor.controller = MotionControlType::velocity;
-  motor.LPF_velocity = 0.5;
+  motor.LPF_velocity = 0.05;
   // motor.PID_velocity.output_ramp = 1000;
-  motor.motion_downsample = 3;
+  motor.motion_downsample = 0;
   // init motor hardware
+  
 motor.linkSensor(&new_sensor);
 //A - is the ID of drive
 
 motor.linkCurrentSense(&current_sense);
-motor.sensor_direction= Direction::UNKNOWN;
-
+// motor.sensor_direction= Direction::UNKNOWN;
+new_sensor.electric_rotations = 0;
 motor.init();
 current_sense.init();
 
@@ -198,7 +197,7 @@ void loop() {
       // float tmp_f = recievedSpeed * float(i);
       // if (tmp_f < -200) tmp_f = -200;
       // if (tmp_f > 200) tmp_f = 200;
-      motor.move(-150);
+      motor.move(500);
       // motor.current_limit = max_current;
       // motor.PID_velocity.P = 0.75;
       // motor.PID_velocity.I = 0.1;
